5c653637569607f517a5305399891123136a070e
Mark Krauzowicz <30778577+mlkrauz@users.noreply.github.com>
2021-11-03 17:13:50 -0400

    Added seeds. (#43)

---
+++ server/seeders/colorData.js
@@ -0,0 +1,16 @@
+const colorData = [
+  {
+    id: '6182ba5aaf0dfdbfbdc4600d',
+    color: '#FF0000',
+  },
+  {
+    id: '6182ba6baf0dfdbfbdc4600f',
+    color: '#00FF00',
+  },
+  {
+    _id: '6182ba72af0dfdbfbdc46011',
+    color: '#0000FF',
+  },
+];
+
+export default colorData;
