9e34b15c0f1b654e420f6ee3c58bd2a38bd09b55
Puma Prince <christopherbreen22@gmail.com>
2021-11-03 16:41:58 -0400

    started making the homepage color picker

---
+++ client/src/components/ColorPickerHome.js
@@ -0,0 +1,20 @@
+import React, { usestate } from 'react';
+
+
+
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
+
+
+const ColorPickerHome = () => (
+    <Card border="dark" style={{width: '18rem'}}>
+      <Button variant="outline-dark">Save style</Button>
+    </Card>
+)
+
+
+
+export default ColorPickerHome;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ColorPickerHome from "../components/ColorPickerHome";
 
 const Home = () => {
   return (
     <div className="container">
      This is the homepage
+     ColorPickerHome();
     </div>
   );
 };
