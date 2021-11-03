48ff749f3c875a351c3a551751c1d29deceeedca
Puma Prince <christopherbreen22@gmail.com>
2021-11-03 17:54:25 -0400

    changed the nav bar so it looked prettier

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -8,17 +8,18 @@ import Nav from 'react-bootstrap/Nav';
 function Navigation() {
 
   return (
-    <Navbar bg="dark" variant="dark">
+      
+    <Navbar  bg="dark" variant="dark" >
       <Container>
-        <Navbar.Brand href="/">Color-Coded</Navbar.Brand>
-        <Nav className="me-auto">
+        <Navbar.Brand className="justify-content-start" href="/">Color-Coded</Navbar.Brand>
+        <Nav className="justify-content-end ">
           <Nav.Link href="/design">Design</Nav.Link> 
           <Nav.Link href="/export">Export</Nav.Link> 
           <Nav.Link href="/login">Login</Nav.Link>  
           </Nav>
       </Container>
     </Navbar>
-
+  
 
   )
 }
