f35db6c15dbc67f65af59117fcc05d654178c91d
Puma Prince <christopherbreen22@gmail.com>
2021-11-02 21:28:51 -0400

    added a nav bar that links to other pages. added the skript to the html so it would work

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React, { usestate } from 'react';
+
+
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/esm/Container';
+import Nav from 'react-bootstrap/Nav';
+
+function Navigation() {
+
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/home">Color-Coded</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/design">Design</Nav.Link> 
+          <Nav.Link href="/export">Export</Nav.Link> 
+          <Nav.Link href="/signup">Signup</Nav.Link>
+          <Nav.Link href="/login">Login</Nav.Link>  
+          </Nav>
+      </Container>
+    </Navbar>
+
+
+  )
+}
+export default Navigation;
