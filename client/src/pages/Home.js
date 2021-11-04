eb0f454702b0030e240a809e154d5bc53a3f6911
Puma Prince <christopherbreen22@gmail.com>
2021-11-04 17:29:42 -0400

    spell check and slight change to padding

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,11 +5,11 @@ import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
-    <><Card className="pt-4 " border="dark" style={{ width: '18rem' }}>
-      <Card.Title className="pb-3"> Welcome to Color-Coded</Card.Title>
-      <Card.Text>Color-Coded is a color selcting app that helps the aesthetically challenged. Our app allows users to pick colors to create a theme, save said theme in their profile, and export the themes that have been created. Using this app you too can become a master of color.   </Card.Text>
-    </Card><Card border="dark" style={{ width: '18rem' }}>
-        <Button variant="outline-dark">Save style</Button>
+    <><Card className="pt-2 " border="dark" style={{ width: '18rem' }}>
+      <Card.Title className="pb-3 m-4"> Welcome to Color-Coded</Card.Title>
+      <Card.Text className="pb-4 mx-4">Color-Coded is a color-selecting app that helps the aesthetically challenged. Our app allows users to pick colors to create a theme, save theme in their profile, and export the themes that have been created. Using this app you too can become a master of color.   </Card.Text>
+    {/* </Card><Card border="dark" style={{ width: '18rem' }}>
+        <Button variant="outline-dark">Save style</Button> */}
       </Card>
     </>
   );
