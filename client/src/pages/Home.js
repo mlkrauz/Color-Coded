8b9f329fa81cfe1c826635456430fb8e346d3ae8
Puma Prince <christopherbreen22@gmail.com>
2021-11-04 17:16:52 -0400

    added a new layout so things look better, changed layout of color bank, and made login and signup pretty

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,8 +5,8 @@ import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
-    <><Card border="dark" style={{ width: '18rem' }}>
-      <Card.Title> Welcome to Color-Coded</Card.Title>
+    <><Card className="pt-4 " border="dark" style={{ width: '18rem' }}>
+      <Card.Title className="pb-3"> Welcome to Color-Coded</Card.Title>
       <Card.Text>Color-Coded is a color selcting app that helps the aesthetically challenged. Our app allows users to pick colors to create a theme, save said theme in their profile, and export the themes that have been created. Using this app you too can become a master of color.   </Card.Text>
     </Card><Card border="dark" style={{ width: '18rem' }}>
         <Button variant="outline-dark">Save style</Button>
