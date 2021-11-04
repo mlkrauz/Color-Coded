9d03b0e63f22fa12f278519e19931e4167660c3f
Puma Prince <christopherbreen22@gmail.com>
2021-11-03 20:27:25 -0400

    spiced up the text for the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <><Card border="dark" style={{ width: '18rem' }}>
       <Card.Title> Welcome to Color-Coded</Card.Title>
-      <Card.Text>Lorem ipsem Dolar, color coded is the best app ever</Card.Text>
+      <Card.Text>Color-Coded is a color selcting app that helps the aesthetically challenged. Our app allows users to pick colors to create a theme, save said theme in their profile, and export the themes that have been created. Using this app you too can become a master of color.   </Card.Text>
     </Card><Card border="dark" style={{ width: '18rem' }}>
         <Button variant="outline-dark">Save style</Button>
       </Card></>
