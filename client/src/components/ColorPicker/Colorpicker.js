ff794fd8d8b505dab3425ff4cc6a75ec5d18dc90
Mark Krauzowicz <30778577+mlkrauz@users.noreply.github.com>
2021-11-05 13:30:21 -0400

    Colorpicker (#71)
    * Added to colorpicker
    
    * Got the color picker working, and sending state to its parent!

--- client/src/components/ColorPicker/Colorpicker.js
+++ client/src/components/ColorPicker/Colorpicker.js
@@ -1,14 +1,53 @@
 import React, { useState } from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
+import IroColorPicker from '../ColorPicker/IroColorPicker'
+
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/esm/Container';
+import Stack from 'react-bootstrap/Stack'
+import Ratio from 'react-bootstrap/Ratio'
+import Button from 'react-bootstrap/Button'
 
-function Colorpicker() {
-
+function Colorpicker(props) {
+  
+  const [state, setstate] = useState({
+    iroColor: '#ffffff',
+    newColor: '#ffffff'
+  });
 
   return (
-    <Card fluid className="p-3 m-2">
-     the color picker will go inside here
+    <Card fluid>
+      <Stack direction="horizontal" gap={3}>
+        <div md='5' className='d-flex justify-content-center overflow-hidden'  style={{ width: '8rem', height: '35rem' }}>
+          <Stack direction="vertical" className='mx-3 my-3 d-flex justify-content-center'>
+            <Button className='my-2' variant="secondary">🔼</Button>
+            <Ratio className='my-2' aspectRatio='1x1'>
+              <div className='d-flex justify-content-center' style={{'background': '#FF0000'}}>
+              </div>
+            </Ratio>
+            <Ratio className='my-2' aspectRatio='1x1'>
+              <div className='d-flex justify-content-center align-items-center' style={{'background': '#00FF00'}}>
+              ✔️
+              </div>
+            </Ratio>
+            <Ratio className='my-2' aspectRatio='1x1'>
+              <div style={{'background': '#0000FF'}}>
+              </div>
+            </Ratio>
+            <Ratio className='my-2' aspectRatio='1x1'>
+              <div style={{'background': '#000000'}}>
+              </div>
+            </Ratio>
+            <Button className='my-2' variant="secondary">🔽</Button>
+          </Stack>
+        </div>
+        <Container md='9' className='d-flex justify-content-center'>
+          <IroColorPicker
+            newColor={state.newColor}
+            onChangeCallback={(colorString) => setstate({ ...state, iroColor: colorString })}
+          />
+        </Container>
+      </Stack>
     </Card>
   );
 }
---
+++ client/src/components/ColorPicker/IroColorPicker.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useRef } from 'react';
+import iro from '@jaames/iro';
+
+const IroColorPicker = ({ newColor, onChangeCallback }) => {
+  // useRef binds data to an HTML element from what I understand.
+  const ref = useRef(null);
+  const colorPickerRef = useRef(null);
+
+  // Because we have passed 0 dependencies to useEffect, this will only render once!
+  // Exactly what we want.
+  useEffect(() => {
+    // We only want to create a single color picker.
+    const iroSettings = {
+      // Set the size of the color picker
+      width: 180,
+      color: '#FFFFFF',
+      layout: [
+        { 
+          component: iro.ui.Wheel,
+        },
+        { 
+          component: iro.ui.Slider,
+          options: {
+            sliderType: 'hue'
+          },
+        },
+        { 
+          component: iro.ui.Slider,
+          options: {
+            sliderType: 'saturation'
+          }
+        },
+        { 
+          component: iro.ui.Slider,
+          options: {
+            sliderType: 'value'
+          }
+        },
+      ]
+    };
+
+    // Create iro color picker and assign to reference
+    colorPickerRef.current = 
+      new iro.ColorPicker(ref.current, {
+        ...iroSettings
+      }
+    )
+
+    // Using iro's inbuilt .on function, we can send the updated color string to the parent's state.
+    colorPickerRef.current.on('color:change', (newColor) => onChangeCallback(newColor.hexString))
+  }, [])
+
+  // We can create a second useEffect to update the iro color picker when we want to render a new color.
+  useEffect(() => {
+    colorPickerRef.current.color.rgb = newColor;
+  }, [newColor])
+
+  // Return an empty <div> with our iro color picker bound to it.
+  return (
+    <div ref={ref}></div>
+  )
+}
+
+export default IroColorPicker;
